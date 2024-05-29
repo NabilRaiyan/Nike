23b613bc9cd09655246b35aadf5be39bf5674e01
NabilRaiyan <rraiyan77@gmail.com>
2024-05-29 20:06:32 +0600

    adding service section

---
+++ src/components/ServiceCard.jsx
@@ -0,0 +1,11 @@
+
+
+const ServiceCard = ({imgURL, label, subtext}) => {
+  return (
+    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[10px] shadow-3xl">
+      section
+    </div>
+  )
+}
+
+export default ServiceCard
