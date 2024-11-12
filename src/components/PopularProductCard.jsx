3987be568f8476da77619ca9561d66f09041e4bd
NabilRaiyan <rraiyan77@gmail.com>
2024-11-12 21:41:07 +0600

    creating MIT License

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,6 +1,7 @@
 import { star } from "../assets/icons"
 
 
+// Popular product card
 const PopularProductCard = ({imgURL, name, price}) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
